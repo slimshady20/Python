1a66d790a507c17800820d5ffb09d6f04c999fc6
양정욱 <61002791+slimshady20@users.noreply.github.com>
2020-06-30 18:36:10 +0900

    30/06/2020

--- contacts/miner.py
+++ contacts/miner.py
@@ -7,6 +7,7 @@ from nltk import FreqDist
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
+
 class Model:
     def __init__(self):
         self._context = ''
@@ -34,9 +35,38 @@ class Model:
 
 class Service:
     def __init__(self):
-        pass
+        self.texts = []
+        self.tokens = []
+        self.okt = Okt()
+        self.stopwords = []
+        self.freqtxt = []
+
+    def extract_token(self, payload):
+        print('>> text문서에서 token 추출')
+        filename = payload.context = payload.fname
+        with open(filename, 'r', encoding='utf-8') as f:
+            self.texts = f.read()
+        print(f'{self.texts[:300]}')
 
 
 class Controller:
     def __init__(self):
         pass
+
+    def download_dictionary(self):
+        nltk.download('all')
+
+
+def print_menu():
+    print('0.Exit')
+    print('1. 사전 다운로드\n')
+    return input('메뉴선택\n')
+
+
+app = Controller()
+while 1:
+    menu = print_menu()
+    if menu == '1':
+        app.download_dictionary()
+    elif menu == '0':
+        break
