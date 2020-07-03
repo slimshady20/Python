4da43943b91a1898615fdd0eb404c9a6c27a6213
양정욱 <61002791+slimshady20@users.noreply.github.com>
2020-07-03 18:51:25 +0900

    03/07/2020

--- miner.py
+++ miner.py
@@ -1,11 +1,11 @@
 from dataclasses import dataclass
-import re
-
 import nltk
 from nltk.tokenize import word_tokenize
 from konlpy.tag import Okt
-import pandas as pd
 from nltk import FreqDist
+
+import pandas as pd
+import re
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
@@ -45,49 +45,66 @@ class Service:
         self.freqtxt = []
 
     def extract_texts(self, payload):
-        print('>>corpus 에서 token 추출')
-        filename = payload.context = payload.fname
+        print('1. corpus 에서 token 추출')
+        filename = payload.context + payload.fname
         with open(filename, 'r', encoding='utf-8') as f:
             self.texts = f.read()
         print(f'1단계 결과물 : {self.texts[:300]}')
 
     def tokenize(self):
-        print('1. corpus에서 한글 추출')
-        texts = self.texts.replace('\n',' ')
+        print('2. corpus 에서 한글 추출')
+        texts = self.texts.replace('\n', ' ')
         tokenizer = re.compile(r'[^ㄱ-힣]')
-        # ^는 not 과 start 두가지 개념이 있음
-        # [^]는 not, ^[]은 start 의미로 표현됨
-        self.texts = tokenizer.sub(' ', texts)  # 한글이 아닌것은 ' ' 처리해서 한글만 남겨라.
-        print(f' 2단계 결과물: {self.texts[:300]}')
+        # ^ 는 not 과 start 두가지 개념이 있음
+        # [^] 은 not, ^[] 은 start 의미로 표현됨
+        self.texts = tokenizer.sub(' ', texts)
+        # 한글이 아닌 것은 ' ' 처리해서 한글과 띄어쓰기 남겨라
+        print(f'2단계 결과물 : {self.texts[:300]}')
 
     def conversion_token(self):
         print('3. 한글 token 변환')
         self.tokens = word_tokenize(self.texts)
-        print(f' 3단계 결과물: {self.texts[:300]}')
+        print(f'3단계 결과물 : {self.tokens[:300]}')
 
     def compound_noun(self):
-        print(' 4. 복합 명사화')
+        print('4. 복합명사화 ')
         arr_ = []
         for token in self.tokens:
             token_pos = self.okt.pos(token)
             _ = [txt_tags[0] for txt_tags in token_pos
                  if txt_tags[1] == 'Noun']
             if len("".join(_)) > 1:
-                self.noun_tokens.append("".join(_))
-            self.noun_tokens = "".join(arr_)
-            print(f'4단계 결과물 : {self.noun_tokens[:300]}')
+                arr_.append("".join(_))
+        self.noun_tokens = " ".join(arr_)
+        print(f'4단계 결과물 : {self.noun_tokens[:300]}')
 
-    def extract_stopword(self):
-        print('5. 노이즈 코퍼스에서 token 추출')
+    def extract_stopword(self, payload):
+        print('5. 노이즈 코퍼스에서 토큰 추출')
+        filename = payload.context + payload.fname
+        with open(filename, 'r', encoding='utf-8') as f:
+            self.stopwords = f.read()
+            print(f'5단계 결과물: {self.stopwords}')
 
     def filtering_text_with_stopword(self):
         print('6. 노이즈 필터링 후 시그널 추출')
+        self.noun_tokens = word_tokenize(self.noun_tokens)
+        self.noun_tokens = [text for text in self.noun_tokens
+                            if text not in self.stopwords]
 
     def frequent_text(self):
         print('7. 시그널 중에 사용빈도 정렬')
+        self.freqtxt = pd.Series(dict(FreqDist(self.noun_tokens))).sort_values(ascending=False)
+        print(f'{self.freqtxt[:10]}')
 
-    def wordcloud(self):
+    def wordcloud(self, payload):
         print('8. 시각화')
+        fname = payload.context + payload.fname
+        wcloud = WordCloud(fname, relative_scaling=0.2, background_color='white') \
+            .generate(" ".join(self.noun_tokens))
+        plt.figure(figsize=(12, 12))
+        plt.imshow(wcloud, interpolation='bilinear')
+        plt.axis('off')
+        plt.show()
 
 
 class Controller:
@@ -106,17 +123,18 @@ class Controller:
         service.tokenize()
         service.conversion_token()
         service.compound_noun()
-        service.extract_stopword()
+        entity.fname = 'stopwords.txt'
+        service.extract_stopword(entity)
         service.filtering_text_with_stopword()
         service.frequent_text()
-        service.wordcloud()
+        entity.fname = 'D2Coding.ttf'
+        service.wordcloud(entity)
 
 
 def print_menu():
-    print('0.Exit')
+    print('0. Exit\n')
     print('1. 사전 다운로드\n')
-    print('2. 데이터 분석')
-    return input('메뉴선택\n')
+    return input('메뉴 선택\n')
 
 
 app = Controller()
